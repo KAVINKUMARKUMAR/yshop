674bdbeb73cb392a713cd54ebe4d0d0f55164c3e
KAVINKUMARKUMAR <kavinkumar2112202@gmail.com>
2025-04-09 07:39:56 +0530

    newly

--- mypro/mainapp/urls.py
+++ mypro/mainapp/urls.py
@@ -1,7 +1,13 @@
 from django.urls import  path
+
 from . import views
 
 urlpatterns = [
     path('',views.homeView ,name="homepage"),
-    path('about',views.aboutView, name='aboutpage')
+    path('about',views.aboutView, name='aboutpage'),
+    path('contact', views.contactView, name = 'contactpage'),
+    path('products/<int:pk>',views.ProductDetials.as_view(),name ='prod_details'),
+    path('products/add', views.AddProduct.as_view(), name='add_product'),
+    path('products/edit/<int:pk>', views.EditProduct.as_view(),name= 'edit_Product'),
+    path('products/del/<int:pk>', views.DelProduct.as_view(),name='del_product'),
 ]
--- mypro/mainapp/views.py
+++ mypro/mainapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
 from django.template import loader
+from django.http import JsonResponse
 
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Product
 # Create your views here.
 def homeView(request):
@@ -11,9 +14,37 @@ def homeView(request):
 
     }
     return HttpResponse(template.render(context,request))
+
+class ProductDetials(DetailView):
+    model = Product
+    template_name = 'product_details.html'
 def aboutView(request):
-    template=loader.get_template('index.html')
+    template=loader.get_template('about.html')
     context={
 
     }
     return HttpResponse(template.render(context,request))
+
+def contactView(request):
+    template = loader.get_template('contact.html')
+    context = {}
+    return HttpResponse(template.render(context,request))
+
+
+class AddProduct(CreateView):
+    model = Product
+    template_name = 'add_product.html'
+    fields = '__all__'
+    success_url = '/'
+
+class EditProduct(UpdateView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'edit_product.html'
+    fields = ['img','price', 'stock', 'desc']
+    success_url = '/'
+
+class DelProduct(DeleteView):
+    model = Product
+    template_name = 'del_product.html'
+    success_url = '/'
